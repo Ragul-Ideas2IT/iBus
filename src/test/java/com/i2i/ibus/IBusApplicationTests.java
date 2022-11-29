0244f6c0cf9880ac9e0ca09b8d9ed2175d83cf7c
ragul <ragul.venkatesan@ideas2it.com>
2022-11-29 19:52:56 +0530

    Spring initializer commit

---
+++ src/test/java/com/i2i/ibus/IBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i2i.ibus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
