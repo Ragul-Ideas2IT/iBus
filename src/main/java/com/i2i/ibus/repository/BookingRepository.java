af462137ed9dc3b57c6730c1315eaf4282caf342
Esakkiraja1837 <esakkiraja.esakkipandi@ideas2it.com>
2022-12-12 17:15:42 +0530

    comments implement

--- src/main/java/com/i2i/ibus/repository/BookingRepository.java
+++ src/main/java/com/i2i/ibus/repository/BookingRepository.java
@@ -1,23 +1,26 @@
 package com.i2i.ibus.repository;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import com.i2i.ibus.model.Booking;
 
 /**
+ * Used to save and retirve the booking Details.
+ * 
  * @author Esakkiraja E
  * @version 1.0
  *
  * @created Nov 29 2022
  */
 @Repository
-public interface BookingRepository extends JpaRepository<Booking, Integer>{
+public interface BookingRepository extends JpaRepository<Booking, Integer> {
     
+    /*
+     * method used to find all user by userId
+     * 
+     * @param userId 
+     * @return a list of  Bookings.
+     */
     List<Booking> findAllByUserId(int userId);
 }
